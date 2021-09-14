b75e8e5f1ef1de2593eaf5d99246371139fee482
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-14 23:15:03 +0000

    updated function format in getCourses

--- functions/helpers/getCourses.js
+++ functions/helpers/getCourses.js
@@ -1,15 +1,13 @@
 const { table } = require('./airtable');
 const formattedReturn = require('./formattedReturn');
 module.exports = async (event) => {
-    try {
-        const courses = await table.select().firstPage();
-        const formattedCourses = courses.map((course) => ({
-            id: course.id,
-            ...course.fields,
-        }));
-        return formattedReturn(200, formattedCourses);
-    } catch (err) {
-        console.error(err);
-        return formattedReturn(500, {});
-    }
+    const courses = await table.select().firstPage();
+    const formattedCourses = courses.map((course) => ({
+        id: course.id,
+        ...course.fields,
+    }));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(formattedReturn),
+    };
 };
