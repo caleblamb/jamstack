13cf3eb387383666edb768c55307cbfc19238e08
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-14 23:39:53 +0000

    reverted function change

--- functions/helpers/getCourses.js
+++ functions/helpers/getCourses.js
@@ -1,13 +1,15 @@
 const { table } = require('./airtable');
 const formattedReturn = require('./formattedReturn');
 module.exports = async (event) => {
-    const courses = await table.select().firstPage();
-    const formattedCourses = courses.map((course) => ({
-        id: course.id,
-        ...course.fields,
-    }));
-    return {
-      statusCode: 200,
-      body: JSON.stringify(formattedReturn),
-    };
+    try {
+        const courses = await table.select().firstPage();
+        const formattedCourses = courses.map((course) => ({
+            id: course.id,
+            ...course.fields,
+        }));
+        return formattedReturn(200, formattedCourses);
+    } catch (err) {
+        console.error(err);
+        return formattedReturn(500, {});
+    }
 };
