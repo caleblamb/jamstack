68c94f5044854e5422e30f831914575672a3b76c
EC2 Default User <ec2-user@ip-172-31-38-226.ec2.internal>
2021-09-15 00:02:25 +0000

    removed dot env

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
     process.env.AIRTABLE_BASE_ID
